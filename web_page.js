49a93ff6eacd1e86c58d931995e78f9112d91b1c
Bo <bo.seok.jeong@gmail.com>
2023-03-09 10:43:22 +0900

    webs

---
+++ web_page.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    alert("나 눌렀어");
+})
+
+let red_btn = document.getElementById("red_btn");
+let content = document.getElementById("content");
+
+red_btn.addEventListener("click",()=>{
+    content.style.background="red";
+
+})
